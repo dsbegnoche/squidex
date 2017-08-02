e318eceac3d0de3813a4ce2a1ab15a5456ba12af
Nathan Isom <nisom@xpanxion.com>
2017-08-02 13:33:26 -0500

    add initial tag field, no input validation

---
+++ src/Squidex.Domain.Apps.Core/Schemas/TagFieldEditor.cs
@@ -0,0 +1,15 @@
+﻿// ==========================================================================
+//  StringFieldEditor.cs
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex Group
+//  All rights reserved.
+// ==========================================================================
+
+namespace Squidex.Domain.Apps.Core.Schemas
+{
+    public enum TagFieldEditor
+    {
+        Input,
+    }
+}
---
+++ src/Squidex.Domain.Apps.Core/Schemas/TagFieldProperties.cs
@@ -0,0 +1,32 @@
+﻿// ==========================================================================
+//  StringFieldProperties.cs
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex Group
+//  All rights reserved.
+// ==========================================================================
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using Newtonsoft.Json.Linq;
+using Squidex.Infrastructure;
+
+// ReSharper disable ObjectCreationAsStatement
+
+namespace Squidex.Domain.Apps.Core.Schemas
+{
+    [TypeName("TagField")]
+    public sealed class TagFieldProperties : FieldProperties
+    {
+        public override JToken GetDefaultValue()
+        {
+            return "";
+        }
+
+        protected override IEnumerable<ValidationError> ValidateCore()
+        {
+            yield break;
+        }
+    }
+}
