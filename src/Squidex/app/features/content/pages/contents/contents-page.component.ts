4de6ca93a380e28a5943a73febf846971824b3bc
Alex Van Dyke <vandyke@civicplus.com>
2017-08-16 07:54:42 -0500

    User Story #26971 - Add Tags to Content Items
    Bug #27611 - Tags should not be the listed column in the content screen
    https://civicplus.tpondemand.com/entity/27611
    Fixing webpack

--- src/Squidex/app/features/content/pages/contents/contents-page.component.ts
+++ src/Squidex/app/features/content/pages/contents/contents-page.component.ts
@@ -197,7 +197,7 @@ export class ContentsPageComponent extends AppComponentBase implements OnDestroy
         this.contentFields = this.schema.fields.filter(x => x.properties.isListField);
 
         if (this.contentFields.length === 0 && this.schema.fields.length > 0) {
-            if (this.schema.fields[0].name == "tags" && this.schema.fields.length > 1) {
+            if (this.schema.fields[0].name === 'tags' && this.schema.fields.length > 1) {
                 this.contentFields = [this.schema.fields[1]];
             } else {
                 this.contentFields = [this.schema.fields[0]];
