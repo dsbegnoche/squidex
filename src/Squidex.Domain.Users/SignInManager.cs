a98f6fe3247a3ac52a2d60dc2fdfb8523f6053db
Kevin McRell <mcrell@civicplus.com>
2017-08-22 11:25:08 -0500

    HCMS Feature #26581 - HCMS User Story #26599 - Integrate with CPP https://civicplus.tpondemand.com/entity/26599
    Try X on single sign out

--- src/Squidex.Domain.Users/SignInManager.cs
+++ src/Squidex.Domain.Users/SignInManager.cs
@@ -4,11 +4,13 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -31,5 +33,22 @@ namespace Squidex.Domain.Users
 
 			return externalLogin;
 		}
-	}
+
+	    public override async Task SignInAsync(IUser user, AuthenticationProperties authenticationProperties, string authenticationMethod = null)
+	    {
+		    var externalLogin = await GetExternalLoginInfoWithDisplayNameAsync();
+		    var userPrincipal = await CreateUserPrincipalAsync(user);
+		    // Review: should we guard against CreateUserPrincipal returning null?
+		    if (authenticationMethod != null)
+		    {
+			    userPrincipal.Identities.First().AddClaim(new Claim(ClaimTypes.AuthenticationMethod, authenticationMethod));
+		    }
+
+			userPrincipal.Identities.First().AddClaim(new Claim("sid", externalLogin.Principal.FindFirstValue("sid")));
+
+		    await Context.Authentication.SignInAsync("Identity.Application",
+			    userPrincipal,
+			    authenticationProperties ?? new AuthenticationProperties());
+		}
+    }
 }
--- src/Squidex/Config/Identity/CivicPlusIdentityUsage.cs
+++ src/Squidex/Config/Identity/CivicPlusIdentityUsage.cs
@@ -36,8 +36,6 @@ namespace Squidex.Config.Identity
 					civicPlusOptions.Scope.Add(scope);
 				}
 
-				app.UseCookieAuthentication();
-
 				app.UseOpenIdConnectAuthentication(civicPlusOptions);
 			}
 
