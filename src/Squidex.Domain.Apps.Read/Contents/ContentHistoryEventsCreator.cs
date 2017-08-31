5bf84aeb929127c25e74b96f5c51fddcf222b798
Keith Wyss <wyss@civicplus.com>
2017-08-31 12:57:05 -0500

    HCMS Feature #27498 - Workflows User Story #27522 - Allow Submitted Items to be Published and Declined https://civicplus.tpondemand.com/entity/27522

--- src/Squidex.Domain.Apps.Read/Contents/ContentHistoryEventsCreator.cs
+++ src/Squidex.Domain.Apps.Read/Contents/ContentHistoryEventsCreator.cs
@@ -33,6 +33,9 @@ namespace Squidex.Domain.Apps.Read.Contents
 
             AddEventMessage<ContentUnpublished>(
                 "unpublished content element.");
+
+            AddEventMessage<ContentDeclined>(
+                "declined content element.");
         }
 
         protected override Task<HistoryEventToStore> CreateEventCoreAsync(Envelope<IEvent> @event)
