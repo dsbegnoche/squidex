e77dcdbe0ff931824b208f7ca529cebe9076c6e8
Alex Van Dyke <vandyke@civicplus.com>
2017-09-14 12:59:03 -0500

    Added a test to the service.
    Feature #28267 - Patterns/Validations
    User Story #27286 - Add Default Patterns
    Bug #28467 - Valid Zip Code not working
    https://civicplus.tpondemand.com/entity/28467

--- src/Squidex/app/shared/utils/text-analytics.spec.ts
+++ src/Squidex/app/shared/utils/text-analytics.spec.ts
@@ -10,50 +10,58 @@ import {
     TextAnalyticsService
 } from './../';
 
-describe('TextAnalyticsService', () => {
+describe('TextAnalyticsService',
+    () => {
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            imports: [
-                HttpClientTestingModule
-            ],
-            providers: [
-                TextAnalyticsService,
-                { provide: ApiUrlConfig, useValue: new ApiUrlConfig('https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases') }
-            ]
+        beforeEach(() => {
+            TestBed.configureTestingModule({
+                imports: [
+                    HttpClientTestingModule
+                ],
+                providers: [
+                    TextAnalyticsService,
+                    {
+                        provide: ApiUrlConfig,
+                        useValue: new ApiUrlConfig(
+                            'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases')
+                    }
+                ]
+            });
         });
-    });
 
-    afterEach(inject([HttpTestingController], (httpMock: HttpTestingController) => {
-        httpMock.verify();
-    }));
-
-    it('should make a post request with correct key',
-        inject([TextAnalyticsService, HttpTestingController],
-            (textAnalyticsService: TextAnalyticsService, httpMock: HttpTestingController) => {
-
-                let tags: string[];
-
-                textAnalyticsService.getKeyPhrases('only article will return')
-                    .then((result: string[]) => {
-                        tags = result;
-                    });
-
-                const req = httpMock.expectOne('https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases');
-
-                expect(req.request.method).toEqual('POST');
-                expect(req.request.headers.get('Ocp-Apim-Subscription-Key')).toEqual('489ad04269e24d9481ac546f4c027e67');
-
-                req.flush({
-                    'documents': [
-                        {
-                            'keyPhrases': [
-                                'article'
-                            ],
-                            'id': 'id'
-                        }
-                    ],
-                    'errors': []
-                });
+        afterEach(inject([HttpTestingController],
+            (httpMock: HttpTestingController) => {
+                httpMock.verify();
             }));
-});
+
+        it('should make a post request with correct key',
+            inject([TextAnalyticsService, HttpTestingController],
+                (textAnalyticsService: TextAnalyticsService, httpMock: HttpTestingController) => {
+
+                    let tags: string[];
+
+                    textAnalyticsService.getKeyPhrases('only article will return')
+                        .then((result: string[]) => {
+                            tags = result;
+                        });
+
+                    const req = httpMock.expectOne(
+                        'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases');
+
+                    expect(req.request.method).toEqual('POST');
+                    expect(req.request.headers.get('Ocp-Apim-Subscription-Key'))
+                        .toEqual('489ad04269e24d9481ac546f4c027e67');
+
+                    req.flush({
+                        'documents': [
+                            {
+                                'keyPhrases': [
+                                    'article'
+                                ],
+                                'id': 'id'
+                            }
+                        ],
+                        'errors': []
+                    });
+                }));
+    });
