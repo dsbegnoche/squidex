ee3d1335bc1ff3aa635a961823d93177d2215328
Nathan Isom <nathanisom27@gmail.com>
2017-09-27 15:04:02 -0500

    add missing header

--- src/Squidex.Infrastructure/Assets/AssetUtil.cs
+++ src/Squidex.Infrastructure/Assets/AssetUtil.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// ==========================================================================
+//  AssetUtil.cs
+//  CivicPlus implementation of Squidex Headless CMS
+// ==========================================================================
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
