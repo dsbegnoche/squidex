b7144e71f2f1bab80505fadbb8ded475357c00a2
Derek Begnoche <begnoche@civicplus.com>
2017-09-29 09:46:12 -0500

    Feature #26583 - Tools User Story #28485 - Implement EventStore https://civicplus.tpondemand.com/entity/28485
    Start of Azure Table Storage

---
+++ src/Squidex.Infrastructure.Azure/CQRS/Events/TableEvent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Squidex.Infrastructure.CQRS.Events
+{
+    public class TableEvent : TableEntity
+    {
+        public string EventPosition { get; set; }
+
+        public long EventStreamNumber { get; set; }
+
+        public EventData Data { get; set; }
+
+        public StoredEvent ToStoredEvent()
+        {
+            return new StoredEvent(EventPosition, EventStreamNumber, Data);
+        }
+    }
+}
---
+++ src/Squidex.Infrastructure.Azure/CQRS/Events/TableStorageEventStore.cs
@@ -0,0 +1,111 @@
+﻿// ==========================================================================
+//  TableStorageEventStore.cs
+//  CivicPlus implementation of Squidex Headless CMS
+// ==========================================================================
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
+using Streamstone;
+
+namespace Squidex.Infrastructure.CQRS.Events
+{
+    public class TableStorageEventStore : IEventStore
+    {
+        private const int AnyVersion = int.MinValue;
+        private readonly CloudTable table;
+        private readonly string prefix;
+
+        public TableStorageEventStore(CloudTable table, string prefix)
+        {
+            this.prefix = prefix?.Trim(' ', '-').WithFallback("squidex");
+            this.table = table;
+        }
+
+        public Task AppendEventsAsync(Guid commitId, string streamName, ICollection<EventData> events)
+        {
+            return AppendEventsAsync(commitId, streamName, -1, events);
+        }
+
+        public async Task AppendEventsAsync(Guid commitId, string streamName, int expectedVersion, ICollection<EventData> events)
+        {
+            Guard.GreaterEquals(expectedVersion, -1, nameof(expectedVersion));
+            var i = expectedVersion;
+
+            IEnumerable<TableEvent> tableEvents = events.Select(x => new TableEvent
+            {
+                Data = x
+            });
+            // iterate through current aggregate events increasing version with each processed event
+            foreach (var @event in tableEvents)
+            {
+                i++;
+                @event.EventPosition = i.ToString();
+                @event.EventStreamNumber = i;
+            }
+
+            var partition = new Partition(table, GetStreamName(streamName));
+
+            var existent = await Stream.TryOpenAsync(partition);
+            var stream = existent.Found
+                ? existent.Stream
+                : new Stream(partition);
+
+            if (stream.Version != expectedVersion)
+            {
+                throw new Exception();
+            }
+
+            try
+            {
+                await Stream.WriteAsync(stream, tableEvents.Select(ToEventData).ToArray());
+            }
+            catch (ConcurrencyConflictException e)
+            {
+                throw new Exception();
+            }
+
+            foreach (var @event in events)
+            {
+                // publish current event to the bus for further processing by subscribers
+                //_publisher.Publish(@event);
+            }
+        }
+
+        public IEventSubscription CreateSubscription()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IReadOnlyList<StoredEvent>> GetEventsAsync(string streamName)
+        {
+            var partition = new Partition(table, GetStreamName(streamName));
+
+            if (!(await Stream.ExistsAsync(partition)))
+            {
+                throw new KeyNotFoundException();
+            }
+
+            return (await Stream.ReadAsync<TableEvent>(partition)).Events.Select(x => x.ToStoredEvent()).ToList();
+        }
+
+        private static Streamstone.EventData ToEventData(TableEvent @event)
+        {
+            var id = Guid.NewGuid();
+            var properties = new
+            {
+                Id = id,
+                Type = @event.Data.Type,
+                Data = JsonConvert.SerializeObject(@event)
+            };
+            return new Streamstone.EventData(EventId.From(id), EventProperties.From(properties));
+        }
+
+        private string GetStreamName(string streamName)
+        {
+            return $"{prefix}-{streamName}";
+        }
+    }
+}
