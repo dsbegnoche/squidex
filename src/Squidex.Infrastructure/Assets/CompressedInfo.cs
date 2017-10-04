0620465fc375c6af45f073d36c13e09f22754e08
Nathan Isom <nathanisom27@gmail.com>
2017-10-04 09:48:53 -0500

    Add compressed info reference
    Feature #26582 - Assets
    User Story #28114 - Compress Image Files
    https://civicplus.tpondemand.com/entity/28114

---
+++ src/Squidex.Infrastructure/Assets/CompressedInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Squidex.Infrastructure.Assets
+{
+    public sealed class CompressedInfo
+    {
+        public int PixelWidth { get; }
+
+        public int PixelHeight { get; }
+
+        public long FileSize { get; }
+
+        public CompressedInfo(int pixelWidth, int pixelHeight, long fileSize)
+        {
+            Guard.GreaterThan(pixelWidth, 0, nameof(pixelWidth));
+            Guard.GreaterThan(pixelHeight, 0, nameof(pixelHeight));
+            Guard.GreaterThan(pixelHeight, 0, nameof(fileSize));
+
+            PixelWidth = pixelWidth;
+            PixelHeight = pixelHeight;
+            FileSize = fileSize;
+        }
+    }
+}
