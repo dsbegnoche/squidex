55098197f20de720fa17594d39467a22769d8f0f
Derek Begnoche <begnoche@civicplus.com>
2017-10-17 15:27:24 -0500

    User Story #28233 - Index Content from Mongo https://civicplus.tpondemand.com/entity/28233

--- src/Squidex.Domain.Apps.Read.Elastic/Contents/ElasticContentRepository.cs
+++ src/Squidex.Domain.Apps.Read.Elastic/Contents/ElasticContentRepository.cs
@@ -9,6 +9,7 @@ using Squidex.Domain.Apps.Read.Schemas.Services;
 using Squidex.Infrastructure;
 using Squidex.Infrastructure.CQRS.Events;
 using Squidex.Infrastructure.Dispatching;
+using Squidex.Domain.Apps.Events.Contents;
 
 namespace Squidex.Domain.Apps.Read.MongoDb.Contents
 {
@@ -36,13 +37,35 @@ namespace Squidex.Domain.Apps.Read.MongoDb.Contents
 
         public Task ClearAsync()
         {
-            //Delete all content indexes to be rebuilt.
+            // Delete all content indexes to be rebuilt.
             throw new NotImplementedException();
         }
 
         public Task On(Envelope<IEvent> @event)
         {
             return this.DispatchActionAsync(@event.Payload, @event.Headers);
+        }
+
+        // On(AppCreated) => CreateIndex?
+
+        protected Task On(ContentCreated @event, EnvelopeHeaders headers)
+        {
+            return ForSchemaAsync(@event.AppId.Id, @event.SchemaId.Id, (collection, schema) =>
+            {
+                return collection.CreateAsync(@event, headers, content =>
+                {
+                    content.SchemaId = @event.SchemaId.Id;
+
+                    SimpleMapper.Map(@event, content);
+                    content.Status = Status.Draft;
+
+                    var idData = @event.Data?.ToIdModel(schema.SchemaDef, true);
+
+                    content.DataText = idData?.ToFullText();
+                    content.DataDocument = idData?.ToBsonDocument();
+                    content.ReferencedIds = idData?.ToReferencedIds(schema.SchemaDef);
+                });
+            });
         }
     }
 }
