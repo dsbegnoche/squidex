c4b0c16bdd243fb710a7c4c7443313e930edf9c7
Derek Begnoche <begnoche@civicplus.com>
2017-10-17 14:59:32 -0500

    User Story #28233 - Index Content from Mongo https://civicplus.tpondemand.com/entity/28233

---
+++ src/Squidex.Domain.Apps.Read.Elastic/Contents/ElasticContentRepository.cs
@@ -0,0 +1,48 @@
+﻿// ==========================================================================
+//  ElasticContentRepository.cs
+//  CivicPlus implementation of Squidex Headless CMS
+// ==========================================================================
+
+using System;
+using System.Threading.Tasks;
+using Squidex.Domain.Apps.Read.Schemas.Services;
+using Squidex.Infrastructure;
+using Squidex.Infrastructure.CQRS.Events;
+using Squidex.Infrastructure.Dispatching;
+
+namespace Squidex.Domain.Apps.Read.MongoDb.Contents
+{
+    public class ElasticContentRepository : IEventConsumer
+    {
+        private readonly ISchemaProvider schemas;
+
+        public ElasticContentRepository(string index, ISchemaProvider schemas)
+        {
+            Guard.NotNull(index, nameof(index));
+            Guard.NotNull(schemas, nameof(schemas));
+
+            this.schemas = schemas;
+        }
+
+        public string Name
+        {
+            get { return GetType().Name; }
+        }
+
+        public string EventsFilter
+        {
+            get { return "^(content-)|(app-)|(asset-)"; }
+        }
+
+        public Task ClearAsync()
+        {
+            //Delete all content indexes to be rebuilt.
+            throw new NotImplementedException();
+        }
+
+        public Task On(Envelope<IEvent> @event)
+        {
+            return this.DispatchActionAsync(@event.Payload, @event.Headers);
+        }
+    }
+}
